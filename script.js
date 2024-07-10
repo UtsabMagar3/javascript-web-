05632afd547afc35144bac7982be5198c409d759
UtsabMagar3 <utsabthamimagar@gmail.com>
2024-07-10 21:59:06 +0545

    added aimple function

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// var number1 = '123';
+// var number2 = 12;
+
+// alert(number1);
+
+
+// var string1 = 'script loaded';
+
+// var num1 = 5;
+// var num2 = 6;
+// var num3 = num1 + num2
+
+// alert(num3)
+
+
+function scriptloaded(){
+    var first = "Utsab"
+    var second = "thami"
+    alert(first + second);
+}
+
+scriptloaded();
+
+function multi_div(n1, n2){
+    mul = n1 * n2
+    div = n1 / n2
+    alert(mul)
+    alert(div)
+}
+
+multi_div(10, 5)
+        
+
+      
+       
+      
